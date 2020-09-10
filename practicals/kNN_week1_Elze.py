a9ab381ecf71aa20ae11bd3c2aaafd0fb1b8b40b
Elze <e.j.s.swinkels@student.tue.nl>
2020-09-10 19:02:53 +0200

    Update kNN_week1_Elze.py
    MSE and accuracy implemented myself io used from sklearn and removed unnecessary code

--- practicals/kNN_week1_Elze.py
+++ practicals/kNN_week1_Elze.py
@@ -1,9 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets import load_diabetes, load_breast_cancer
-from math import sqrt
-import sys
-from sklearn.metrics import mean_squared_error, accuracy_score
 
 # Normalize data
 def norm(data):
@@ -18,7 +15,7 @@ def norm(data):
 def eucl_dist(row1, row2):
     # take each feature as one dimension so loop over all dimensions
 	distance = sum(np.subtract(row1,row2)**2)  # use L2 norm
-	return sqrt(distance)
+	return distance**(0.5)
 
 # Get the k nearest neighbors and the values of their features
 def get_kNN(traindata, data_point, k):
@@ -37,11 +34,25 @@ def pred(traindata, traintargets, testdata, k):
     NN, NN_data = get_kNN(traindata, testdata, k)
     y_NN = [traintargets.item(i) for i in NN]
     prediction = max(set(y_NN), key=y_NN.count)
-    return prediction
+    return prediction #prediction.reshape((prediction.shape[0],1))
 
+def mse(y_true, y_pred):
+    mse = np.average((y_true - y_pred) ** 2,axis=0,weights=None)
+    return mse
+
+def accuracy(y_true, y_pred):
+    nr_samples = y_true.shape[0]
+    # when abs(y_test - y_pred) returns 1, the prediction is false (negative or positive)
+    # the sum of this is the number of false negatives and positives:
+    FNandFP = sum(abs(y_true - y_pred)) 
+    TNandTP = nr_samples - FNandFP
+    acc = TNandTP / nr_samples
+    return acc
+    
 # Load data
 breast_cancer = load_breast_cancer()
 
+""" randomly selecting the train samples still has to be implemented !!""" 
 # split data into train and test and use all features
 nr_samples = breast_cancer.data.shape[0]
 train_frac = 2/3
@@ -50,27 +61,12 @@ X_train = breast_cancer.data[:cutoff, :]
 y_train = breast_cancer.target[:cutoff, np.newaxis]
 X_test = breast_cancer.data[cutoff:, :]
 y_test = breast_cancer.target[cutoff:, np.newaxis]
-# or let it select a particular amount of train samples at random?
 
-k = 3 # number of neighbors you want to include
-
+# normalize data
 X_train_n = norm(X_train)
 X_test_n = norm(X_test)
 
-y_pred = np.zeros((X_test.shape[0]))    # create empty array
-y_pred_n = np.zeros((X_test.shape[0]))  # create empty array
-for test_sample in range(X_test.shape[0]):
-    # fill array with predicted labels
-    y_pred[test_sample] = pred(X_train, y_train, X_test[test_sample], k) 
-    y_pred_n[test_sample] = pred(X_train_n, y_train, X_test_n[test_sample], k) 
-    #y_pred = y_pred.reshape((y_pred.shape[0],1))   leads to the same results as without reshaping
-    #y_pred_n = y_pred_n.reshape((y_pred_n.shape[0],1))
-    
-print("Mean squared error with normalization for k = %i : %.2f" % (k,mean_squared_error(y_test, y_pred_n)))
-print("Mean squared error without normalization for k = %i : %.2f" % (k,mean_squared_error(y_test, y_pred)))
-print("Accuracy with normalization for k = %i : %.2f" % (k,accuracy_score(y_test, y_pred_n)))
-print("Accuracy without normalization for k = %i : %.2f" % (k,accuracy_score(y_test, y_pred)))
-
+# Calculate mean square error and accuracy for nr_k nearest neighbors
 nr_k = 40
 y_pred_k = np.zeros((X_test.shape[0],nr_k))
 MSE = np.zeros((nr_k))
@@ -78,16 +74,10 @@ acc = np.zeros((nr_k))
 for k in range(1,nr_k+1):
     for test_sample in range(X_test.shape[0]):
         y_pred_k[test_sample,k-1] = pred(X_train_n, y_train, X_test_n[test_sample], k)
-    MSE[k-1] = mean_squared_error(y_test,y_pred_k[:,k-1])
-    acc[k-1] = accuracy_score(y_test,y_pred_k[:,k-1])
-
-# controle: is the same
-MSE2 = np.zeros((nr_k))
-acc2 = np.zeros((nr_k))
-for k in range(0,nr_k):
-    y_pred_this_k = y_pred_k[:,k]
-    MSE2[k] = mean_squared_error(y_test,y_pred_this_k.reshape((y_pred_this_k.shape[0],1)))
-    acc2[k] = accuracy_score(y_test,y_pred_this_k.reshape((y_pred_this_k.shape[0],1)))
+    y_pred_this_k = y_pred_k[:,k-1]
+    # reshape y_pred_this_k because it has another shape than y_test
+    MSE[k-1] = mse(y_test,y_pred_this_k.reshape((y_pred_this_k.shape[0],1)))  
+    acc[k-1] = accuracy(y_test,y_pred_this_k.reshape((y_pred_this_k.shape[0],1)))
 
 
 # Plot in figure
